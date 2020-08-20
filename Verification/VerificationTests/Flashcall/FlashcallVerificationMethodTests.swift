43a09b97571a6a6e423d47e9d05c0d4024a53d54
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2020-08-20 09:34:15 +0200

    Resolves #VERIOS-34 basic podspec file created (content to be discussed)

--- Verification/VerificationTests/Flashcall/FlashcallVerificationMethodTests.swift
+++ Verification/VerificationTests/Flashcall/FlashcallVerificationMethodTests.swift
@@ -14,6 +14,7 @@ class FlashcallVerificationMethodTests: XCTestCase {
     
     func testCorrectFlashcallInitiationResponseNotifiesListener() throws {
         try createHelperWithFlashcallMethod().testCorrectInitiationResponseNotifiesInitListenerWithProperData()
+        
     }
     
     func testErrorResponseNotifiesInitListenerWithApiError() throws {
