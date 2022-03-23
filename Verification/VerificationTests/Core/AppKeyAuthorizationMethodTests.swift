dd972c4e90d90fad2f282a52c9246b45e900a6a3
Aleksander Wojcik <aleksander.wojcik@sinch.com>
2022-03-23 09:28:56 +0100

    Tests update

--- Verification/VerificationTests/Core/AppKeyAuthorizationMethodTests.swift
+++ Verification/VerificationTests/Core/AppKeyAuthorizationMethodTests.swift
@@ -10,17 +10,28 @@ import XCTest
 @testable import Verification
 
 class AppKeyAuthorizationMethodTests: XCTestCase {
-
-    static let TEST_KEY = "key"
-    static let TEST_KEY_HEADER = "Application \(TEST_KEY)"
-
-    func testAuthHeaderIncludedInRequest() throws {
-        let authMethod = AppKeyAuthorizationMethod(appKey: AppKeyAuthorizationMethodTests.TEST_KEY)
-        let request = URLRequest(url: URL(string: "http://localhost.com")!)
-        let modifiedRequest = authMethod.onAuthorize(request)
-        XCTAssertTrue(modifiedRequest.allHTTPHeaderFields?.contains(where: { key, value in
-            key == "Authorization" && value == AppKeyAuthorizationMethodTests.TEST_KEY_HEADER
-        }) ?? false , "Modified request contains wrong authorization header.")
-    }
-
+  
+  static let TEST_KEY = "key"
+  static let TEST_KEY_HEADER = "Application \(TEST_KEY)"
+  
+  func testAuthHeaderIncludedInRequest() throws {
+    let authMethod = AppKeyAuthorizationMethod(appKey: AppKeyAuthorizationMethodTests.TEST_KEY)
+    let request = URLRequest(url: URL(string: "http://sinch.com")!)
+    let modifiedRequest = authMethod.onAuthorize(request)
+    XCTAssertTrue(modifiedRequest.allHTTPHeaderFields?.contains(where: { key, value in
+      key == "Authorization" && value == AppKeyAuthorizationMethodTests.TEST_KEY_HEADER
+    }) ?? false , "Modified request contains wrong authorization header.")
+  }
+  
+  func testAuthHeaderNotIncludedInRequestIfSinchNotPresent() throws {
+    let authMethod = AppKeyAuthorizationMethod(appKey: AppKeyAuthorizationMethodTests.TEST_KEY)
+    let request = URLRequest(url: URL(string: "http://example.com")!)
+    let modifiedRequest = authMethod.onAuthorize(request)
+    let allHeaders = modifiedRequest.allHTTPHeaderFields ?? [:]
+    XCTAssertFalse(allHeaders.contains(where: { key, value in
+      key == "Authorization" && value == AppKeyAuthorizationMethodTests.TEST_KEY_HEADER
+    }) , "Modified request contains authorization header that should not be included")
+  }
+  
 }
+
