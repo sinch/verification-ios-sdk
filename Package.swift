7591b88ab4cad3bb6f10ab5e77548e2695d75e3e
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2023-06-06 12:46:51 +0200

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 let package = Package(
     name: "SinchVerificationSDK",
-    platforms: [.iOS(.v12),
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "Verification", targets: ["Verification"])
     ],
