69a55d050ae37da270e03896b2ae90e14fbfd28b
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2023-06-06 10:44:24 +0200

    fix package

--- Package.swift
+++ Package.swift
@@ -3,12 +3,12 @@
 import PackageDescription
 let package = Package(
     name: "SinchVerificationSDK",
+    products: [
+        .library(name: "Verification", targets: ["Verification"])
+    ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.4")),
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0"),
-    ],
-    products: [
-        .library(name: "Verification", targets: ["Verification"])
     ],
     targets: [
         .target(name: "Verification", dependencies: ["Alamofire", "CocoaLumberjack"], path: "Verification/Verification/Classes"),
