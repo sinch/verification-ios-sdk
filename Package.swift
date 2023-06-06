807dfdde9ec04fea0de621785c06be9485cb07e4
Aleksander Wójcik <aleksander.wojcik@sinch.com>
2023-06-06 10:57:27 +0200

    Add reachability

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.6.4")),
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0"),
-        .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.4.0")
+        .package(url: "https://github.com/marmelroy/PhoneNumberKit", from: "3.4.0"),
+        .package(url: "https://github.com/ashleymills/Reachability.swift", from: "5.1.0")
     ],
     targets: [
         .target(name: "Verification", dependencies: ["Alamofire", "CocoaLumberjack", "PhoneNumberKit"], path: "Verification/Verification/Classes"),
